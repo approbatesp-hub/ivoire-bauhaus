1c49e2c8c55331f055d89baad84a8f0237c23484
GuedeD <gdfa95@gmail.com>
2026-01-29 12:21:21 +0000

    second push

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,10 +1,3 @@
-// PHOTOS IMMO
-import Photo1 from "../assets/galerie/photo1.png";
-import Photo2 from "../assets/galerie/photo2.png";
-import Photo3 from "../assets/galerie/photo3.png";
-import Photo4 from "../assets/galerie/photo4.png";
-import Photo5 from "../assets/galerie/photo5.png";
-
 // PHOTOS CAMION
 import photoCam1 from "../assets/camion/camion.jpeg";
 import photoCam2 from "../assets/camion/camion1.jpeg";
@@ -21,11 +14,6 @@ import photoSupp8 from "../assets/panneaux/support8.png";
 import photoSupp9 from "../assets/panneaux/support9.png";
 import photoSupp10 from "../assets/panneaux/support10.png";
 
-// UNIVERSITES
-import photoUniv1 from "../assets/universite/univ1.jpeg";
-import photoUniv2 from "../assets/universite/univ2.jpeg";
-import photoUniv3 from "../assets/universite/univ3.jpeg";
-import photoUniv4 from "../assets/universite/univ4.jpeg";
 import {
   Building2,
   FileText,
@@ -36,13 +24,6 @@ import {
 } from "lucide-react";
 import toast from "react-hot-toast";
 
-export const galleryImages = [
-  { url: Photo1, alt: "Photo 1" },
-  { url: Photo2, alt: "Photo 2" },
-  { url: Photo3, alt: "Photo 3" },
-  { url: Photo4, alt: "Photo 4" },
-  { url: Photo5, alt: "Photo 5" },
-];
 export const galleryImages2 = [
   { url: photoCam1, alt: "Photo 1" },
   { url: photoCam2, alt: "Photo 2" },
@@ -58,13 +39,6 @@ export const galleryImages3 = [
   { url: photoSupp8, alt: "Photo 8" },
   { url: photoSupp9, alt: "Photo 9" },
   { url: photoSupp10, alt: "Photo 10" },
-];
-
-export const galleryImages4 = [
-  { url: photoUniv1, alt: "Photo 1" },
-  { url: photoUniv2, alt: "Photo 2" },
-  { url: photoUniv3, alt: "Photo 3" },
-  { url: photoUniv4, alt: "Photo 4" },
 ];
 
 export const sections = [
